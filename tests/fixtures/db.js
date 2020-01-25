5345b218f6ab7ae56490d4c3270d04adc814427a
Hernan <hernanlogran@hotmail.com>
2020-01-25 17:47:05 -0300

    Create tests for POST /games/gameId/cells/cellId/reveal

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -2,6 +2,7 @@
 
 const db = require('../../src/integration/db.js');
 const Game = require('../../src/integration/models/game.js');
+const {CELL_STATES} = require('../../src/constants');
 
 module.exports = {
   clearGames: () => {
@@ -12,7 +13,47 @@ module.exports = {
   },
   createTestGame: () => {
     const game = new Game(5, 5, 0);
+
+    game.cells[3].hasBomb = true;
+    game.cells[8].hasBomb = true;
+    game.cells[13].hasBomb = true;
+    game.cells[18].hasBomb = true;
+    game.cells[23].hasBomb = true;
+    game.cells[4].state = CELL_STATES.REVEALED;
+    game.cells[9].state = CELL_STATES.TAGGED_FLAG;
+    game.cells[14].state = CELL_STATES.TAGGED_QUESTION_MARK;
+
+    /* Test game
+    +---+---+---+---+---+
+    |   |   |   | B | R |
+    +---+---+---+---+---+
+    |   |   |   | B | F |
+    +---+---+---+---+---+
+    |   |   |   | B | ? |
+    +---+---+---+---+---+
+    |   |   |   | B |   |
+    +---+---+---+---+---+
+    |   |   |   | B |   |
+    +---+---+---+---+---+
+    */
+
+    game.bombs = 5;
     db.games.push(game);
     return game;
   },
+  createAboutToWinGame: () => {
+    const game = new Game(1, 2, 0);
+
+    game.cells[1].hasBomb = true;
+
+    /* About-to-win Game
+    +---+---+
+    |   | B |
+    +---+---+
+    */
+
+    game.bombs = 1;
+    db.games.push(game);
+    return game;
+  }
 };
--- tests/games.test.js
+++ tests/games.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const app = require('../src/api/app');
 const server = request(app);
 const db = require('./fixtures/db');
+const {CELL_STATES, GAME_STATES} = require('../src/constants');
 
 describe('POST /games', () => {
 
@@ -78,8 +79,142 @@ describe('GET /games/:gameId', () => {
 
   test('If game is not found returns error 404', async() => {
     await server
-      .get('/games/NON_EXISTING_ID')
+      .get('/games/NOT_EXISTING_ID')
       .expect(404)
       .expect('Content-Type', /json/);
   });
+});
+
+describe('POST /games/:gameId/cells/:cellId/reveal', () => {
+  let testGame;
+
+  beforeEach(() => {
+    db.clearGames();
+    testGame = db.createTestGame();
+  });
+
+  test('If game is not found returns error 404', async() => {
+    const gameId = 'NOT_EXISTING_GAME';
+    const cellId = 'NOT_EXISTING_CELL';
+    await server
+      .post(`/games/${gameId}/cells/${cellId}/reveal`)
+      .expect(404)
+      .expect('Content-Type', /json/);
+  });
+
+  test('If cell is not found returns error 404', async() => {
+    const gameId = testGame.id;
+    const cellId = 'NOT_EXISTING_CELL';
+    await server
+      .post(`/games/${gameId}/cells/${cellId}/reveal`)
+      .expect(404)
+      .expect('Content-Type', /json/);
+  });
+
+  test('Can reveal a hidden cell', async() => {
+    const gameId = testGame.id;
+    const cellId = 0;
+    const {body: game} = await server
+      .post(`/games/${gameId}/cells/${cellId}/reveal`)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    expect(game.cells[cellId].state).toBe(CELL_STATES.REVEALED);
+  });
+
+  test('Revealed cells remain revealed', async() => {
+    const gameId = testGame.id;
+    const cellId = 4;
+    const {body: game} = await server
+        .post(`/games/${gameId}/cells/${cellId}/reveal`)
+        .expect(200)
+        .expect('Content-Type', /json/);
+    expect(game.cells[cellId].state).toBe(CELL_STATES.REVEALED);
+  });
+
+  test('Can reveal a cell tagged with flag', async() => {
+    const gameId = testGame.id;
+    const cellId = 9;
+    const {body: game} = await server
+        .post(`/games/${gameId}/cells/${cellId}/reveal`)
+        .expect(200)
+        .expect('Content-Type', /json/);
+    expect(game.cells[cellId].state).toBe(CELL_STATES.REVEALED);
+  });
+
+  test('Can reveal a cell tagged with question mark', async() => {
+    const gameId = testGame.id;
+    const cellId = 14;
+    const {body: game} = await server
+        .post(`/games/${gameId}/cells/${cellId}/reveal`)
+        .expect(200)
+        .expect('Content-Type', /json/);
+    expect(game.cells[cellId].state).toBe(CELL_STATES.REVEALED);
+  });
+
+  test('When a cell with no adjacent mines is revealed, all adjacent squares will be revealed (and repeat)', async() => {
+    const gameId = testGame.id;
+    const cellId = 0;
+    const {body: game} = await server
+        .post(`/games/${gameId}/cells/${cellId}/reveal`)
+        .expect(200)
+        .expect('Content-Type', /json/);
+
+    expect(game.cells[0].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[1].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[2].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[3].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[4].state).toBe(CELL_STATES.REVEALED);
+
+    expect(game.cells[5].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[6].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[7].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[8].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[9].state).toBe(CELL_STATES.TAGGED_FLAG);
+
+    expect(game.cells[10].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[11].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[12].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[13].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[14].state).toBe(CELL_STATES.TAGGED_QUESTION_MARK);
+
+    expect(game.cells[15].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[16].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[17].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[18].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[19].state).toBe(CELL_STATES.HIDDEN);
+
+    expect(game.cells[20].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[21].state).toBe(CELL_STATES.REVEALED);
+    expect(game.cells[22].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[23].state).toBe(CELL_STATES.HIDDEN);
+    expect(game.cells[24].state).toBe(CELL_STATES.HIDDEN);
+  });
+
+  describe('Winning and Losing conditions', () => {
+    beforeEach(() => {
+      testGame = db.createAboutToWinGame();
+    });
+
+    test('When bomb is revealed, game is lost', async() => {
+      const gameId = testGame.id;
+      const cellId = 1;
+      const {body: game} = await server
+          .post(`/games/${gameId}/cells/${cellId}/reveal`)
+          .expect(200)
+          .expect('Content-Type', /json/);
+      expect(game.state).toBe(GAME_STATES.LOST);
+    });
+
+    test('On revealing last empty cell, game is won', async() => {
+      const gameId = testGame.id;
+      const cellId = 0;
+      const {body: game} = await server
+          .post(`/games/${gameId}/cells/${cellId}/reveal`)
+          .expect(200)
+          .expect('Content-Type', /json/);
+      expect(game.state).toBe(GAME_STATES.WON);
+    });
+
+  })
 });
