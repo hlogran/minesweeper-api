6de9b9bb327337fc12ac856c4f1ccaffe77e588f
Hernan <hernanlogran@hotmail.com>
2020-01-25 17:08:16 -0300

    Create constants to handle game and cell's states

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    GAME_STATES: {
+        UNINITIALIZED: 'UNINITIALIZED',
+        IN_GAME: 'IN_GAME',
+        WON: 'WON',
+        LOST: 'LOST',
+    },
+    CELL_STATES: {
+        HIDDEN: 'HIDDEN',
+        REVEALED: 'REVEALED',
+        TAGGED_FLAG: 'TAGGED_FLAG',
+        TAGGED_QUESTION_MARK: 'TAGGED_QUESTION_MARK',
+    },
+};
