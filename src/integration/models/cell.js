4384db625f85ec8a5853d3446073573017414ff9
Hernan <hernanlogran@hotmail.com>
2020-01-25 17:25:37 -0300

    Remove id property for cells

--- src/integration/models/cell.js
+++ src/integration/models/cell.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const uuidv1 = require('uuid/v1');
 const {CELL_STATES} = require('../../constants');
 
 module.exports = class Cell {
   constructor(row = 0, col = 0) {
-    this.id = uuidv1();
     this.row = row;
     this.col = col;
     this.hidden = true;
